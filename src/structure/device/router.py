17e60513793dcc251f5be9d61f6bac3958485b56
Alexander Mejía <alexmejia199745@live.com>
2021-02-02 16:51:43 -0500

    Routing Command (#1)
    Added routing command that is accessed from specific router.
    Command allows to check, create and delete routes.
    
    Co-authored-by: alex <alexjk6697@gmail.com>

--- src/structure/device/router.py
+++ src/structure/device/router.py
@@ -4,5 +4,55 @@ from .device import Device
 class Router(Device):
     def __init__(self, device_type, name, address, credentials, status, connected_devices, routes):
         super(Router, self).__init__(device_type, name, address, credentials, status)
+        # connected devices is a set of addresses connected to the router
         self.connected_devices = connected_devices
+        # routes is dict where  key: destionation, value: address to lookup for destination
         self.routes = routes
+        self.programs = {"routes": self.routing}
+
+    def routing(self, action="help", *parameters):
+        output = ""
+
+        if(action == "help"):
+            output = ("To use the routing capabilities, you "
+                      "have the following commands available:\n"
+                      "- routes all -> This will show a list of all the routes this router has\n"
+                      "- routes check [destination]-> This will show "
+                      "the route to get to a destination\n"
+                      "- routes create [destination] [direction] -> "
+                      "This will create a new route\n"
+                      "- routes delete [destination]\n"
+                      "...")
+
+        if(action == "all"):
+            output = ""
+            for key in self.routes:
+                output = output + key + ":" + self.routes[key] + "\n"
+
+        if(action == "check"):
+            if parameters[0] in self.routes.keys():
+                output = "connected by: " + self.routes[parameters[0]]
+            else:
+                raise Exception(
+                    "Route does not exist here"
+                )
+
+        if(action == "delete"):
+            if parameters[0] in self.routes.keys():
+                del self.routes[parameters[0]]
+                output = parameters[0] + " successfully deleted"
+            else:
+                raise Exception(
+                    "Route does not exist here"
+                )
+
+        if(action == "create"):
+            if parameters[0] in self.routes.keys():
+                raise Exception(
+                    "Route already exist, espcify another one"
+                )
+            else:
+                self.routes[parameters[0]] = parameters[1]
+                output = parameters[0] + " connected by " + parameters[1]
+
+        return output
